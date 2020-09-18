01a97ab7e81089140be9cfe2a82948bfc390062e
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-18 13:46:35 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>IDを入力してください</p>
+    <input v-model="myId" />
+    <p>{{showWord}}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      ids: {
+        "9Cm2H6bULBtnFKOBMool": 0,
+        "EjF12B6bV3sIfqip9yQH": 1,
+        "KUiVOqJihJX0f53SyNJy": 0,
+        "VeiWjmFllSNlbdN93FXQ": 0,
+        "YHaGGw6iVyVn4Kh80u3G": 0,
+      },
+      words: ["絵本", "漫画"],
+      myId: "",
+    };
+  },
+
+  computed: {
+    showWord() {
+      return this.words[this.ids[this.myId]];
+    },
+  },
+  components: {},
+};
 </script>
 
 <style>
